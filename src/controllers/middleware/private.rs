8ec9ae401f212b1648a1f5ae1ceb5e8e1e325da8
afgonzalez-dev <afgonzalezdev@gmail.com>
2025-11-05 21:26:07 -0300

    fmt

--- src/controllers/middleware/private.rs
+++ src/controllers/middleware/private.rs
@@ -157,10 +157,10 @@ mod tests {
     use axum_extra::extract::PrivateCookieJar;
     use axum_test::TestServer;
     use http::header::{HeaderValue, COOKIE};
+    use loco_rs::app::SharedStore;
     use loco_rs::config::{Config, Database, Logger, Server, Workers};
     use loco_rs::controller::middleware::{self};
     use loco_rs::environment::Environment;
-    use loco_rs::app::SharedStore;
     use loco_rs::storage::Storage;
     use loco_rs::{cache, storage};
     use sea_orm::DatabaseConnection;
