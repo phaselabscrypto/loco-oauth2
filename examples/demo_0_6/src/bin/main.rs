b3471a035bba3802a68b16d54b2c04b32d757237
afgonzalez-dev <afgonzalezdev@gmail.com>
2025-11-12 09:04:36 -0300

    Remove demo folder

--- examples/demo_0_6/src/bin/main.rs
+++
@@ -1,8 +0,0 @@
-use demo_0_6::app::App;
-use loco_rs::cli;
-use migration::Migrator;
-
-#[tokio::main]
-async fn main() -> eyre::Result<()> {
-    cli::main::<App, Migrator>().await
-}
--- examples/demo_0_6/tests/requests/mod.rs
+++
@@ -1,6 +0,0 @@
-mod auth;
-mod notes;
-mod prepare_data;
-mod user;
-
-pub mod oauth2;
